eed9ab9b273eeb4743ec6b66bfa09c9815f5995a
Prasiddh Goel <74052423+prasiddhgoel@users.noreply.github.com>
2021-07-02 13:02:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var player, alien1,rocket,bg,astronaut,crops ;
-var alien2,alien3,invi,invi2,tributetorem,bgsong,explosion,explosion1,invi,invi2,loading,loadingsc,gun,gunimg;
+var alien2,alien3,invi,invi2,tributetorem,bgsong,explosion,explosion1,invi,invi2,loading,loadingsc, waterboi;
 var playerimg, alienimg,bgimg,asimg,crops1,a2,a3;
-var bullet
+var bullet, changePower="none"
 var gameState="loading"
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -21,6 +21,7 @@ gunimg=loadImage("image (27).png")
 a3=loadImage("image (17).png")
 bgsong=loadSound("rem.mp3")
 explosion=loadImage("image (18).png")
+waterboi=loadImage("image (29).png")
 loadingsc=loadImage("image (20).png")
 }
 
@@ -42,20 +43,30 @@ gun=createSprite(800,300,50,50)
 
  alien3.addImage("normal alien",a3)
 	engine = Engine.create();
-  player.addImage(playerImage)
+  player.addImage("normal player",playerImage)
+  player.addImage("waterboi",waterboi)
   alien1.addImage(alienimg)
   crops1.addImage(crops)
   astronaut.addImage(asimg)
   alien2.velocityY=2
   alien3.addImage("explosion",explosion)
   loading.addImage(loadingsc)
-  gun.addImage(gunimg)
+  
   invi.visible=false
   bullet.visible=false
+  gun.visible=false
 	world = engine.world;
-  player.debug=true
-  astronaut.debug=true
+ 
   astronaut.setCollider("rectangle",0,0,30,100)
+  alien1.scale=0.1
+  alien2.scale=0.1
+  alien3.scale=0.3
+  crops1.scale=0.3
+  astronaut.scale=0.3
+  loading.scale=1
+  gun.scale=0.5
+  player.scale=0.09
+
 	//Create the Bodies Here.
 
 
@@ -67,30 +78,31 @@ gun=createSprite(800,300,50,50)
 function draw() {
   rectMode(CENTER);
   background(bgimg);
-  player.scale=0.1
-  alien1.scale=0.1
-  alien2.scale=0.1
-  alien3.scale=0.3
-  crops1.scale=0.3
-  astronaut.scale=0.3
-  loading.scale=1
-  gun.scale=0.5
  var edges=createEdgeSprites()
   astronaut.bounceOff(edges)
   alien2.bounceOff(edges)
   alien2.bounceOff(invi)
  // alien2.addAnimation("bluealien",a2);
       if (gameState==="play") {
+        player.scale=0.09
+        if(keyDown(LEFT_ARROW))
+        { changePosition(-5,0);
+      } else if(keyDown(RIGHT_ARROW)){ 
+        changePosition(5,0);
+      } else if(keyDown(UP_ARROW)){
+          changePosition(0,-5);
+      } else if(keyDown(DOWN_ARROW)){
+          changePosition(0,+5);     
+      }
         if (player.isTouching(alien1)) {
           textSize(25);
           fill("green")
           stroke(1)
           text('Hey there! would you mind harvesting crops please? business is tough out here. Press spacebar on the crops',0, 500);
           alien1.lifetime=60
+          changePower="none"
         }
-
-
-       
+     
 
         if (player.isTouching(alien3)) {
           textSize(25);
@@ -98,6 +110,7 @@ function draw() {
           stroke(1)
           text('HOW DARE YOU COME IN MY TERRITORY? I AM GONNA BLAST!!',100, 500);
           alien3.changeImage("explosion",explosion)
+          changePower="none"
         }
 
         if (astronaut.isTouching(player)) {
@@ -111,30 +124,35 @@ function draw() {
         fill("black");
         stroke(1)
         text('GAME OVER! REFRESH THE PAGE TO START PLAYING AGAIN.TRY NOT TOUCHING THE EVIL ALIENS.',0, 500);
+        changePower="none"
         }
 
         if (player.isTouching(alien2)) {
+        changePower="water power"
         textSize(25)
         fill ("blue")
         stroke(1)
-        text("Hi! Thanks for visiting me. No one bothers to come here to visit me. take this gun and shoot the evil alien!", 0, 500)
-        }
+        text("Hi! Thanks for visiting me. No one bothers to come here to visit me. Take these powers and kill the alien!", 0, 500)
+      
 
-        if(keyDown(LEFT_ARROW))
-        { changePosition(-5,0);
-      } else if(keyDown(RIGHT_ARROW)){ 
-        changePosition(5,0);
-      } else if(keyDown(UP_ARROW)){
-          changePosition(0,-5);
-      } else if(keyDown(DOWN_ARROW)){
-          changePosition(0,+5); 
+        }
+        if (changePower==="water power") {
+          player.changeImage("waterboi",waterboi)
+          player.scale=0.3
+          if (player.isTouching(astronaut)) {
+          astronaut.destroy()
+          }
         
-      }
+        }   
+
+      
+      
     keyPressed()
     
-    }
+        }
   drawSprites();
 
+ 
 }
 
 function changePosition(x,y){
@@ -152,18 +170,12 @@ if (keyCode===32&& player.isTouching(crops1)) {
  fill("red");
  stroke(1)
  text('LEVEL 2')
-}              
-
+}
 if(keyCode===13 && gameState==="loading") {
-loading.visible=false
-gameState="play"
-astronaut.velocityX=5
-astronaut.velocityY=5
+  loading.visible=false
+  gameState="play"
+  astronaut.velocityX=5
+  astronaut.velocityY=5
+}
 
-if (keyCode===82) {
-bullet.visible=true
-bullet.velocityY=1
-bullet.velocityX=1
-}
-}
- }                                                                                                                                                 
+}                                                                                                                                                                                                                                                                        
